a08edd60557d2de30f2eb8af4c1bac311c03bff5
Sérgio Emanuel <sergio.junior1605@gmail.com>
2024-12-13 21:04:36 -0300

    Iniciando Projeto 2

---
+++ Projetos/Projeto2/Projeto2_Controle_de_estoque.py
@@ -0,0 +1,36 @@
+# Menu Inicial
+def menu_inicial():
+    print("-"*40)
+    print("[1] - Adicionar produto ")
+    print("[2] - Listar produtos ")
+    print("[3] - Atualizar produto ")
+    print("[4] - Remover produto ")
+    print("[5] - Salvar dados ")
+    print("[6] - Finalizar programa ")
+    print("-"*40)
+
+def adicionar_produto():
+    pass
+
+def listar_produtos():
+    pass
+
+def atualizar_produto():
+    pass
+
+def remover_produto():
+    pass
+
+def salvar_estoque():
+    pass
+
+def abrir_estoque():
+    pass
+
+def controle_de_estoque():
+    pass
+
+
+# Inicio do programa
+if __name__ == "__main__":
+    controle_de_estoque()
