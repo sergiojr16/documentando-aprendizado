4bbf0a5e44db6ce0a65cf51cf2e1e88c997e61db
Sérgio Emanuel <sergio.junior1605@gmail.com>
2024-12-14 08:03:58 -0300

    Implementa função 'adicionar_produto' e organiza função principal

--- Projetos/Projeto2/Projeto2_Controle_de_estoque.py
+++ Projetos/Projeto2/Projeto2_Controle_de_estoque.py
@@ -1,4 +1,3 @@
-# Menu Inicial
 def menu_inicial():
     print("-"*40)
     print("[1] - Adicionar produto ")
@@ -9,10 +8,17 @@ def menu_inicial():
     print("[6] - Finalizar programa ")
     print("-"*40)
 
-def adicionar_produto():
-    pass
+def adicionar_produto(estoque):
+    id_produto = input("Digite o ID do produto: ")
+    nome = input("Digite o nome do produto: ").capitalize()
+    quantidade = int(input("Digite a quantidade deste produto no estoque: "))
+    preco = float(input("Digite o preço da unidade deste produto: "))
 
-def listar_produtos():
+    novo_produto = {"ID": id_produto, "nome": nome, "quantidade": quantidade, "preco": preco}
+    estoque.append(novo_produto)
+    print(f"{nome} foi adicionado ao estoque.")
+
+def listar_produtos(estoque):
     pass
 
 def atualizar_produto():
@@ -28,9 +34,26 @@ def abrir_estoque():
     pass
 
 def controle_de_estoque():
-    pass
+    estoque =[]
+    while True:
+        menu_inicial()
+        opcao = input("Selecione uma opção: ")
+        if opcao == "1":
+            adicionar_produto(estoque)
+        elif opcao == "2":
+            pass
+        elif opcao == "3":
+            pass
+        elif opcao == "4":
+            pass
+        elif opcao == "5":
+            pass
+        elif opcao == "6":
+            print("Finalizando programa...")
+            break
+        else:
+            print("Opção inválida, tente novamente.")
 
 
-# Inicio do programa
 if __name__ == "__main__":
     controle_de_estoque()
