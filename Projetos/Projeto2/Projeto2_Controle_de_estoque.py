fcaadce5faf9bb3cd177fd40c746284dd0117a0c
Sérgio Emanuel <sergio.junior1605@gmail.com>
2024-12-15 08:28:07 -0300

    Adiciona função 'remover produto' e ajusta pequenos erros

--- Projetos/Projeto2/Projeto2_Controle_de_estoque.py
+++ Projetos/Projeto2/Projeto2_Controle_de_estoque.py
@@ -1,6 +1,8 @@
 import os
 def menu_inicial():
-    print("-"*40)
+    print("\n"+"-"*40)
+    print("==== Controle de Estoque ====")
+    print("-" * 40)
     print("[1] - Adicionar produto ")
     print("[2] - Listar produtos ")
     print("[3] - Atualizar produto ")
@@ -14,17 +16,19 @@ def adicionar_produto(estoque):
         id_produto = int(input("Digite o ID do produto: "))
         for produto in estoque:
             if id_produto == produto['ID']:
-                print("Já existe um produto com este ID, tente novamente utilizando um ID diferente.")
+                print("Já existe um produto com este ID, tente novamente utilizando um ID diferente.\n")
+                listar_produtos(estoque)
                 return
         nome = input("Digite o nome do produto: ").capitalize()
         quantidade = int(input("Digite a quantidade deste produto no estoque: "))
         preco = float(input("Digite o preço da unidade deste produto: "))
-
-        novo_produto = {"ID": id_produto, "nome": nome, "quantidade": quantidade, "preco": preco}
-        estoque.append(novo_produto)
-        print(f"\n O produto {nome} foi adicionado ao estoque.")
     except ValueError:
         print("Você digitou um valor inválido. Por favor, tente novamente.")
+        return
+    novo_produto = {"ID": id_produto, "nome": nome, "quantidade": quantidade, "preco": preco}
+    estoque.append(novo_produto)
+    print(f"\n O produto {nome} foi adicionado ao estoque.")
+
 
 def listar_produtos(estoque):
     if not estoque:
@@ -45,9 +49,10 @@ def atualizar_produto(estoque):
             id_produto_atualizado = int(input("\nDigite o ID do produto a ser atualizado: "))
             for produto in estoque:
                 if id_produto_atualizado == produto['ID']:
+                    print("\nOpções:")
                     print("[1] - Nome.")
                     print("[2] - Quantidade.")
-                    print("[3] - Valor.")
+                    print("[3] - Preço.")
                     print("[4] - Retornar ao menu principal ")
                     opcao_atualizar = input("\nSelecione a opção a ser atualizada: ")
                     if opcao_atualizar == "1":
@@ -55,7 +60,7 @@ def atualizar_produto(estoque):
                         produto['nome'] = novo_nome
                         print(f"O nome atualizado do produto de ID {id_produto_atualizado} é: {produto['nome']}")
                     elif opcao_atualizar == "2":
-                        nova_quantidade = input("Digite a quantidade atualizada: ")
+                        nova_quantidade = int(input("Digite a quantidade atualizada: "))
                         produto['quantidade'] = nova_quantidade
                         print(f"A quantidade atualizada do produto de ID {id_produto_atualizado} é: {produto['quantidade']}")
                     elif opcao_atualizar == "3":
@@ -68,12 +73,35 @@ def atualizar_produto(estoque):
                     else:
                         print("Opção inválida, nenhum produto foi atualizado")
                     return
-            print(f"Não foi encontrado nenhum produto com o ID {id_produto_atualizado}.")
         except ValueError:
             print("Você digitou um valor inválido. Por favor, tente novamente.")
+            return
+        print(f"Não foi encontrado nenhum produto com o ID {id_produto_atualizado}.")
 
-def remover_produto():
-    pass
+
+def remover_produto(estoque):
+    listar_produtos(estoque)
+    if not estoque:
+        print("Não há produtos no estoque. ")
+    else:
+        try:
+            id_removido = int(input("\nDigite o ID do produto que deseja remover: "))
+        except ValueError:
+            print("Você digitou um valor inválido. Por favor, tente novamente.")
+            return
+        for produto in estoque:
+            if id_removido == produto['ID']:
+                confirmacao = input(f"\nO produto {produto['nome']} de ID {produto['ID']} será removido "
+                                    f"permanentemente, você confirma a ação? [S]/[N]: ").upper()
+                if confirmacao not in ["S","N"]:
+                    print("\nOpção inválida, tente novamente")
+                elif confirmacao == "N":
+                    print("\nNenhum produto foi removido")
+                else:
+                    estoque.remove(produto)
+                    print(f"\nO produto com ID {produto['ID']} foi removido. ")
+                return
+        print(f"Não foi encontrado nenhum produto com o ID {id_removido}.")
 
 def salvar_estoque():
     pass
@@ -86,7 +114,7 @@ def controle_de_estoque():
     while True:
         menu_inicial()
         opcao = input("Selecione uma opção: ")
-        os.system("cls")
+        os.system('cls')
         if opcao == "1":
             adicionar_produto(estoque)
         elif opcao == "2":
@@ -94,7 +122,7 @@ def controle_de_estoque():
         elif opcao == "3":
             atualizar_produto(estoque)
         elif opcao == "4":
-            pass
+            remover_produto(estoque)
         elif opcao == "5":
             pass
         elif opcao == "6":
@@ -102,6 +130,8 @@ def controle_de_estoque():
             break
         else:
             print("Opção inválida, tente novamente.")
+        input("\n\n\n\nAperte Enter para continuar...")
+        os.system('cls')
 
 
 if __name__ == "__main__":
